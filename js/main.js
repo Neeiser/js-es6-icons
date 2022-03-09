4bad77fb6d2d12a0773b437c0f100d72569d43ee
Neeiser <stortini.study@gmail.com>
2022-03-10 00:02:43 +0100

    End of the guided JS. Filled of comment to explain every steps

--- js/main.js
+++ js/main.js
@@ -114,37 +114,23 @@ const iconsObjArr=
 	}
 ];
 
-console.log(iconsObjArr)
 
-/*
-
-Milestone 1
-Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona,
-in cui è presente il nome dell'icona e l'icona stessa.
-
-Milestone 2
-Ciascuna icona ha una proprietà "color": 
-utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-
-Milestone 3
-Aggiungere alla pagina una select in cui le options corrispondono 
-ai vari tipi di icone (animal, vegetable, user). 
-Quando l'utente seleziona un tipo dalla select, 
-visualizzare solamente le icone corrispondenti. 
-
-*/
-
-const container = document.getElementById('icons-container');
+//PARTENZA!!!
 
 
 /* 
 Spiegazione funzione: 
+
 - Creo una funzione che PER OGNI oggetto nell'arrey madre (iconsObjArr)
   mi crea un div a cui attribuisco la classe box (presente su css)
+
 - Al div appena creato inserisco grazie al'innerHTML tutti i relativi valori,
   specificati ad ogni ciclo grazie all'argomento (objArg)
-- Aggiungo sopra a tutto un innerHTML= "" per pulire la pagina.
+
+- Aggiungo sopra a tutto un innerHTML= "" per pulire la pagina (Utilizzato in seguito).
 */
+
+const container = document.getElementById('icons-container');
 
 function insertIcon (allIconsArr, iconBoxes) {
 
@@ -166,14 +152,58 @@ function insertIcon (allIconsArr, iconBoxes) {
 
 /* 
 Spiegazione AZIONE funzione: 
-- Chiaramente la funzione non si esegue perchè non ho specificato a che variabili
-  essa deve eseguirsi.
+- Chiaramente la funzione non si esegue perchè non ho specificato quali variabili
+  essa deve influenzare.
+
 - Perciò chiamo la funzione e come argomenti passo le 2 variabili:
   .iconsObjArr: in relazione all'argomento precedente 'allIconsArr'
   .container:   in relazione all'argomento precedente 'iconBoxes'
 */
+
 insertIcon (iconsObjArr, container)
 
 
+
+
+
+
+//Ora che ho stampato in pagina tutto, procedo alla logica del FILTRO.
+
+//Seleziono il SELECT già presente nel DOM
 const selector = document.getElementById('icons-type-selector');
 
+
+//Creo un eventListener con funzione "change". Il change agisce perchè ho usato
+//un selector all'interno dell'HTML.
+
+//La funzione espressa qua sotto la userò solo sta volta quindi lascio tutto vuoto.
+selector.addEventListener('change', function() {
+
+	//Creo una costante che agisce sul valore ATTUALMENTE selezionato --> QUESTO (this)
+	const selected = this.value 
+
+	//Se la casella selezionata NON è "ALL" allora:
+	if (selected != 'all') {
+
+		//mi crei una classe che filtra l'arrey principale
+		//NB: .filter agisce filtrando e depositando gli elementi filtrati in un'arrey creato da lui
+		const iconsFiltered = iconsObjArr.filter((argument) => {
+
+			//Se il tipo selezionato è uguale al valore di QUESTA variabile allora...
+			if (argument.type == selected) {
+				return true
+			}
+		});
+
+		//Aziona la funzione creata prima su queste 2 variabili:
+		//-iconsFiltered: il nuovo arrey delle icone filtrate
+		//-container: lo stesso di prima
+		insertIcon(iconsFiltered, container)
+	} else {
+
+		//ALTRIMENTI:
+		//-iconsObjArr: l'arrey con tutte le icone
+		//-container: lo stesso di prima
+		insertIcon(iconsObjArr, container)
+	}
+});
